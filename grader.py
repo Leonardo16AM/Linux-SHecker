2cf4956077a0436cc75c2d51ea81efba86c6ab9e
Leonardo Daniel Artiles Montero <51642689+Leonardo16AM@users.noreply.github.com>
2021-09-17 21:11:39 -0400

    Update grader.py
    Fixed literal grader

--- grader.py
+++ grader.py
@@ -1,5 +1,18 @@
 # To create a custom grader create a function grader(str input,str answer,str output) who returns a boolean if it is a correct answer 
-
+import re
 
 def grader(input,answer,output):
-    return answer==output
+    try:
+        ansli=re.split(' |\n',answer)
+        outli=re.split(' |\n',output)
+        answer=[]
+        output=[]
+        for i in ansli:
+            if i!='' : answer.append(i)
+        for i in outli:
+            if i!='' : output.append(i)
+        print(answer)
+        print(output)
+        return answer==output
+    except Exception:
+        return False
